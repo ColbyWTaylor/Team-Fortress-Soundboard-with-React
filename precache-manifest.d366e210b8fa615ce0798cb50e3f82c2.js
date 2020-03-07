67628d34ade5f2f67fbb948d32549b641d63eafa
C T <colbyt.093@gmail.com>
2020-03-07 15:53:58 -0500

    Updates

---
+++ precache-manifest.d366e210b8fa615ce0798cb50e3f82c2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f93cd5861533ceeeedc6ddbd94bd5e0",
+    "url": "/Team-Fortress-Soundboard-with-React/index.html"
+  },
+  {
+    "revision": "56b9448331fd1c8ba335",
+    "url": "/Team-Fortress-Soundboard-with-React/static/css/main.37d751d3.chunk.css"
+  },
+  {
+    "revision": "1e85a37c7f76925e7a74",
+    "url": "/Team-Fortress-Soundboard-with-React/static/js/2.ac9ebb95.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Team-Fortress-Soundboard-with-React/static/js/2.ac9ebb95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b9448331fd1c8ba335",
+    "url": "/Team-Fortress-Soundboard-with-React/static/js/main.daebd8fc.chunk.js"
+  },
+  {
+    "revision": "3c778b4e441e8e4eb24d",
+    "url": "/Team-Fortress-Soundboard-with-React/static/js/runtime-main.96398d8f.js"
+  },
+  {
+    "revision": "d6f6dd027b6d41e1fbb60779d61d3f2e",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/demoman_taunts01.d6f6dd02.wav"
+  },
+  {
+    "revision": "5779c12f6bb0bdc7ce4459ead7aee492",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/engineer_taunts01.5779c12f.wav"
+  },
+  {
+    "revision": "47012018bcc5928d8e4ac6152ed7d1db",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/heavy_taunts01.47012018.wav"
+  },
+  {
+    "revision": "f506f7795215ae3aff01569294d65e61",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/medic_taunts01.f506f779.wav"
+  },
+  {
+    "revision": "bba845c1c87b55f962073d53c6f09bba",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/pyro_taunts01.bba845c1.wav"
+  },
+  {
+    "revision": "ff6ab53dc7e3ae623f060cc1bbf0f72a",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/scout_taunts01.ff6ab53d.wav"
+  },
+  {
+    "revision": "aa1c5d9c7f56171a022a78da6567a96c",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/sniper_taunts01.aa1c5d9c.wav"
+  },
+  {
+    "revision": "fed4a8ee6b42cb1ac805d62e19fadfd6",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/soldier_taunts01.fed4a8ee.wav"
+  },
+  {
+    "revision": "eb23835c91934fa9715df1c901b94f1b",
+    "url": "/Team-Fortress-Soundboard-with-React/static/media/spy_taunts01.eb23835c.wav"
+  }
+]);
